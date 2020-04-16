9767e1192f0b820d2198f8248572094b7a9dc485
Ivan <ieroshenko@wpi.edu>
2020-04-16 14:37:25 -0400

    Adding origin

---
+++ src/main/java/edu/wpi/teamname/FXMLController.java
@@ -0,0 +1,18 @@
+package edu.wpi.teamname;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FXMLController implements Initializable {
+
+  @FXML private Label lbl;
+  @FXML private Button btnpopup;
+  @FXML private Button btncloseup;
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {}
+}
